89ad7760e2faafd0176af0b5fd470b0345c7abdc
Gi Pyo Kim <gipyok@gmail.com>
2019-12-12 15:29:53 -0800

    implemented the calculator view controller

--- RPNCalculator-objc/RPNCalculator-objc/GIPCalculatorViewController.m
+++ RPNCalculator-objc/RPNCalculator-objc/GIPCalculatorViewController.m
@@ -7,9 +7,14 @@
 //
 
 #import "GIPCalculatorViewController.h"
+#import "GIPCalculator.h"
+#import "GIPDigitAccumulator.h"
+
 
 @interface GIPCalculatorViewController ()
+
 @property (weak, nonatomic) IBOutlet UITextField *textField;
+
 - (IBAction)add:(id)sender;
 - (IBAction)subtract:(id)sender;
 - (IBAction)multiply:(id)sender;
@@ -18,37 +23,85 @@
 - (IBAction)enterDecimal:(UIButton *)sender;
 - (IBAction)saveNumberToCalculator:(UIButton *)sender;
 
+@property (nonatomic) GIPCalculator *calculator;
+@property (nonatomic) GIPDigitAccumulator *digitAccumulator;
+
+- (void)updateCalculator;
+- (void)updateDigit;
+
 @end
 
 @implementation GIPCalculatorViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self.textField setDelegate:self];
+    
+    _calculator = [[GIPCalculator alloc] init];
+    _digitAccumulator = [[GIPDigitAccumulator alloc] init];
 }
 
+- (void)updateCalculator {
+    
+    if ([self.calculator topValue] != NULL) {
+        self.textField.text = [[self.calculator topValue] stringValue];
+    } else {
+        self.textField.text = @"";
+    }
+}
+
+- (void)updateDigit {
+    
+    if ([self.digitAccumulator value]) {
+        self.textField.text = [NSString stringWithFormat:@"%f", ([self.digitAccumulator value])];
+    } else {
+        self.textField.text = @"";
+    }
+}
+
+
 - (IBAction)add:(id)sender {
+    [self.calculator applyOperator:add];
+    [self updateCalculator];
 }
 
 - (IBAction)subtract:(id)sender {
+    [self.calculator applyOperator:subtract];
+    [self updateCalculator];
 }
 
 - (IBAction)multiply:(id)sender {
+    [self.calculator applyOperator:multiply];
+    [self updateCalculator];
 }
 
 - (IBAction)divide:(id)sender {
+    [self.calculator applyOperator:divide];
+    [self updateCalculator];
 }
 
 - (IBAction)enterNumericDigit:(UIButton *)sender {
+    [self.digitAccumulator addDigitWithNumericValue:sender.tag];
+    [self updateDigit];
 }
 
 - (IBAction)enterDecimal:(UIButton *)sender {
+    [self.digitAccumulator addDecimalPoint];
+    [self updateDigit];
 }
 
 - (IBAction)saveNumberToCalculator:(UIButton *)sender {
+    [self.calculator pushNumber:[self.digitAccumulator value]];
+    [self.digitAccumulator clear];
+    [self updateCalculator];
+    [self updateDigit];
 }
 
 - (BOOL)textFieldShouldClear:(UITextField *)textField {
+    [self.calculator clear];
+    [self.digitAccumulator clear];
+    return YES;
 }
     
 @end
