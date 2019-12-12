dbb2f624b43cff767f3e1601c90f35de7cda87a2
Gi Pyo Kim <gipyok@gmail.com>
2019-12-12 14:33:12 -0800

    UI Setup

---
+++ RPNCalculator-objc/RPNCalculator-objc/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  RPNCalculator-objc
+//
+//  Created by Gi Pyo Kim on 12/12/19.
+//  Copyright © 2019 GIPGIP Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
