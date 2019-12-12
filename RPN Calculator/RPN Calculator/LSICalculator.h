ce6b5aac979a030ca567e3d2974789c43ce106b4
Isvvc <lyons@tuta.io>
2019-12-12 14:32:25 -0700

    Create LSICalculator class

---
+++ RPN Calculator/RPN Calculator/LSICalculator.h
@@ -0,0 +1,21 @@
+//
+//  LSICalculator.h
+//  RPN Calculator
+//
+//  Created by Isaac Lyons on 12/12/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LSICalculator : NSObject
+
+typedef NS_ENUM(char, operation) {add, subtract, multiply, divide};
+
+@property (readonly) NSNumber *topValue;
+
+- (void)pushNumber:(double)value;
+- (void)applyOperator:(operation)operator;
+- (void)clear;
+
+@end
