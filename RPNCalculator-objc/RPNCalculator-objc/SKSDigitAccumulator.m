cb7fb1e3c17da5996421eefe30f9bf294fa5472f
Skyler Suzuki <skylersuzuk1@gmail.com>
2020-01-23 13:34:23 -0800

    Added DigitAccumulator files.

---
+++ RPNCalculator-objc/RPNCalculator-objc/SKSDigitAccumulator.m
@@ -0,0 +1,25 @@
+//
+//  SKSDigitAccumulator.m
+//  RPNCalculator-objc
+//
+//  Created by Lambda_School_Loaner_204 on 1/23/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "SKSDigitAccumulator.h"
+
+@implementation SKSDigitAccumulator
+
+- (void)addDigitWithNumericValue:(double)digit {
+    //NSNumber
+}
+
+- (void)addDecimalPoint {
+
+}
+
+- (void)clear {
+
+}
+
+@end
