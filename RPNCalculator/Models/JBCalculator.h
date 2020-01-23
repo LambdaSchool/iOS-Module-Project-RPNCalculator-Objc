b15ed6ad32567113684cc7dc76fdd33a09fd0773
Jon Bash <jonbash@protonmail.com>
2020-01-23 12:44:03 -0800

    add operator enum

---
+++ RPNCalculator/Models/JBCalculator.h
@@ -0,0 +1,17 @@
+//
+//  JBCalculator.h
+//  RPNCalculator
+//
+//  Created by Jon Bash on 2020-01-23.
+//  Copyright © 2020 Jon Bash. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JBCalculator : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RPNCalculator/Models/JBCalculator.m
@@ -0,0 +1,19 @@
+//
+//  JBCalculator.m
+//  RPNCalculator
+//
+//  Created by Jon Bash on 2020-01-23.
+//  Copyright © 2020 Jon Bash. All rights reserved.
+//
+
+#import "JBCalculator.h"
+
+
+typedef NS_ENUM(NSInteger, JBOperator) {
+    add, subtract, multiply, divide
+};
+
+
+@implementation JBCalculator
+
+@end
