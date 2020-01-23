2bab6541aa29c7f80251ea4b3b05e23a295e142b
Jon Bash <jonbash@protonmail.com>
2020-01-23 12:31:42 -0800

    set up boilerplate for stack

---
+++ RPNCalculator/Models/JBStack.m
@@ -0,0 +1,44 @@
+//
+//  JBStack.m
+//  RPNCalculator
+//
+//  Created by Jon Bash on 2020-01-23.
+//  Copyright © 2020 Jon Bash. All rights reserved.
+//
+
+#import "JBStack.h"
+
+@implementation JBStack
+
+- (instancetype)initWithArray:(NSArray *)array
+{
+    self = [super init];
+    if (self) {
+
+    }
+    return self;
+}
+
+- (void)push:(id)newElement
+{
+
+}
+
+- (id)pop
+{
+
+    return nil;
+}
+
+- (id)peek
+{
+
+    return nil;
+}
+
+- (void)clear
+{
+
+}
+
+@end
