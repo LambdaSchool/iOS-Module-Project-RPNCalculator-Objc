c9c464ea352644ad542e42c422b1714ef4b24bb5
John Kouris <jdkouris@gmail.com>
2020-02-06 19:24:00 -0800

    Implemented the methods for the DigitAccumulator

--- RPNCalculator/RPNCalculator/JDKDigitAccumulator.m
+++ RPNCalculator/RPNCalculator/JDKDigitAccumulator.m
@@ -8,6 +8,37 @@
 
 #import "JDKDigitAccumulator.h"
 
+@interface JDKDigitAccumulator ()
+
+@property (nonatomic)NSMutableArray *digits;
+
+@end
+
 @implementation JDKDigitAccumulator
+
+- (instancetype)init
+{
+    if (self = [super init]) {
+        _digits = [[NSMutableArray alloc] init];
+    }
+    return self;
+}
+
+- (void)addDigitWithNumericValue:(NSNumber *)digit
+{
+    double *combinedDigit = &_value;
+    [self.digits addObject:digit];
+    *combinedDigit = [[self.digits componentsJoinedByString:@""] doubleValue];
+}
+
+- (void)addDecimalPoint
+{
+    [self.digits addObject:@"."];
+}
+
+- (void)clear
+{
+    [self.digits removeAllObjects];
+}
 
 @end
