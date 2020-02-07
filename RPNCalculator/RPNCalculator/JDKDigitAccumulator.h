4a9f9452a4ee2c16489daabe7f170cb50b35f7ea
John Kouris <jdkouris@gmail.com>
2020-02-06 18:54:31 -0800

    Added the property and methods to the DigitAccumulator header file

--- RPNCalculator/RPNCalculator/JDKDigitAccumulator.h
+++ RPNCalculator/RPNCalculator/JDKDigitAccumulator.h
@@ -12,6 +12,12 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface JDKDigitAccumulator : NSObject
 
+@property (readonly) double value;
+
+- (void)addDigitWithNumericValue:(NSNumber *)digit;
+- (void)addDecimalPoint;
+- (void)clear;
+
 @end
 
 NS_ASSUME_NONNULL_END
