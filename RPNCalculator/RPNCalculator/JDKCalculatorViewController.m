c9d9958785e656bfc1840566604c5cec28b82e9e
John Kouris <jdkouris@gmail.com>
2020-02-06 19:44:26 -0800

    Added the functionality to perform calculations and have result display in textField

--- RPNCalculator/RPNCalculator/JDKCalculatorViewController.m
+++ RPNCalculator/RPNCalculator/JDKCalculatorViewController.m
@@ -16,9 +16,21 @@
 @property (nonatomic) JDKDigitAccumulator *digitAccumulator;
 @property (nonatomic) NSNumberFormatter *numberFormatter;
 
+- (void)updateTextFieldWithValue:(double)value;
+
 @end
 
 @implementation JDKCalculatorViewController
+
+- (instancetype)initWithCoder:(NSCoder *)coder
+{
+    self = [super initWithCoder:coder];
+    if (self) {
+        _calculator = [[JDKCalculator alloc] init];
+        _digitAccumulator = [[JDKDigitAccumulator alloc] init];
+    }
+    return self;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -27,26 +39,60 @@
 }
 
 - (IBAction)enterNumericDigit:(UIButton *)sender {
-    
+    NSNumber *number = [NSNumber numberWithInteger:sender.tag];
+    [self.digitAccumulator addDigitWithNumericValue:number];
+    [self updateTextFieldWithValue:[self.digitAccumulator value]];
 }
 
 - (IBAction)enterDecimal:(id)sender {
+    [self.digitAccumulator addDecimalPoint];
 }
 
 - (IBAction)addNumberToStack:(id)sender {
+    [self.calculator pushNumber:[self.digitAccumulator value]];
+    [self.digitAccumulator clear];
 }
 
 - (IBAction)addResult:(id)sender {
+    [self.calculator applyOperator:add];
+    [self updateTextFieldWithValue:[self.calculator topValue]];
 }
 
 - (IBAction)subtractResult:(id)sender {
+    [self.calculator applyOperator:subtract];
+    [self updateTextFieldWithValue:[self.calculator topValue]];
 }
 
 - (IBAction)multiplyResult:(id)sender {
+    [self.calculator applyOperator:multiply];
+    [self updateTextFieldWithValue:[self.calculator topValue]];
 }
 
 - (IBAction)divideResult:(id)sender {
+    [self.calculator applyOperator:divide];
+    [self updateTextFieldWithValue:[self.calculator topValue]];
 }
 
+- (void)updateTextFieldWithValue:(double)value
+{
+    NSNumber *placeholderValue = [NSNumber numberWithDouble:value];
+    self.textField.text = [self.numberFormatter stringFromNumber:placeholderValue];
+}
+
+- (NSNumberFormatter *)numberFormatter
+{
+    NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
+    [formatter setAllowsFloats:YES];
+    return formatter;
+}
+
+
+- (BOOL)textFieldShouldClear:(UITextField *)textField
+{
+    [self.calculator clear];
+    [self.digitAccumulator clear];
+    
+    return YES;
+}
 
 @end
